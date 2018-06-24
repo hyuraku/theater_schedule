7f261b8dcaf3f3f70894d8b95aaede032d90203c
hyuraku <yu@hill-wood.com>
2018-06-24 19:50:42 +0900

    メソッドの名前修正

--- app/controllers/api/schedules_controller.rb
+++ app/controllers/api/schedules_controller.rb
@@ -23,7 +23,6 @@ class Api::SchedulesController < ApplicationController
   def get_date_work
     @schedules = Schedule.where(schedule_day:  (Time.zone.today + params[:id].to_i)).where(work_id: params[:work_id])
     render 'show', formats: 'json', handlers: 'jbuilder'
-
   end
 
   def get_thea_work
@@ -31,7 +30,7 @@ class Api::SchedulesController < ApplicationController
     render 'show', formats: 'json', handlers: 'jbuilder'
   end
 
-  def get_all_date
+  def get_all_data
     @schedules = Schedule.where(schedule_day:  (Time.zone.today + params[:id].to_i)).where(theater_id: params[:theater_id]).where(work_id: params[:work_id])
     render 'show', formats: 'json', handlers: 'jbuilder'
   end
