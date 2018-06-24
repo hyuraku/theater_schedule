85c1fd4bef80443dd77a2eb2d38e44c3050edbe6
hyuraku <yu@hill-wood.com>
2018-06-24 19:50:29 +0900

    index.js修正

--- app/javascript/packs/schedules/index.js
+++ app/javascript/packs/schedules/index.js
@@ -5,7 +5,8 @@ import axios from 'axios';
 // 劇場のIDのリスト
 var thea_id_list = []
 var work_id_list = []
-var target = []
+var date_id_list = []
+
 window.onload = function (){
 var theaterIndex =new Vue({
   el: '#js-theaterIndex',
@@ -14,9 +15,49 @@ var theaterIndex =new Vue({
     scheduleInfoBool: false
   },
   methods: {
-    setTheaterInfo(id){
-      thea_id_list.push(id)
-      axios.get(`api/schedules/show/${id}`)
+    setDateInfo(id){
+      date_id_list.push(id)
+      if (thea_id_list.length > 0) {
+        if(work_id_list.length > 0){
+          // get_all_dataのメソッド
+          for (var dateId of date_id_list) {
+            for (var theaId of thea_id_list) {
+              for (var workId of work_id_list) {
+                axios.get(`api/schedules/get_all_data/${dateId}/${theaId}/${workId}`)
+                  .then(res => {
+                    this.scheduleInfo = res.data;
+                    this.scheduleInfoBool = true;
+                  });
+              }
+            }
+          }
+        }
+        else {
+          // get_date_theaのメソッド
+          for (var dateId of date_id_list) {
+            for (var theaId of thea_id_list) {
+                axios.get(`api/schedules/get_date_thea/${dateId}/${theaId}`)
+                  .then(res => {
+                    this.scheduleInfo = res.data;
+                    this.scheduleInfoBool = true;
+                  });
+            }
+          }
+        }
+      } else if (work_id_list.length > 0) {
+        // get_date_workのメソッド
+      }else{
+        for (var dateId of date_id_list) {
+            for (var workId of work_id_list) {
+              axios.get(`api/schedules/get_date_work/${dateId}/${workId}`)
+                .then(res => {
+                  this.scheduleInfo = res.data;
+                  this.scheduleInfoBool = true;
+                });
+            }
+        }
+      }
+      axios.get(`api/schedules/get/${id}`)
         .then(res => {
           this.scheduleInfo = res.data;
           this.scheduleInfoBool = true;
@@ -30,9 +71,9 @@ var theaterIndex =new Vue({
         this.scheduleInfoBool = true;
       });
     },
-    setDateInfo(id){
-      target.push(id)
-      axios.get(`api/schedules/get/${id}`)
+    setTheaterInfo(id){
+      thea_id_list.push(id)
+      axios.get(`api/schedules/show/${id}`)
       .then(res => {
         this.scheduleInfo = res.data;
         this.scheduleInfoBool = true;
@@ -40,6 +81,4 @@ var theaterIndex =new Vue({
     }
   }
 });
-
-console.log(target)
 }
