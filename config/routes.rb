3d9082d66542aa9a064147f8dea51d3fdf0fec65
hyuraku <yu@hill-wood.com>
2018-06-24 19:50:12 +0900

    ルート修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,14 @@ Rails.application.routes.draw do
   namespace :api do
    resources :schedules
    # get "schedules/show/:theater_id/:work_id/" => "schedules#show"
+
+   get "schedules/get/:id/" => "schedules#get"
    get "schedules/show/:theater_id/" => "schedules#show"
    get "schedules/index/:work_id/" => "schedules#index"
-   get "schedules/get/:id/" => "schedules#get"
 
    get "schedules/get_date_thea/:id/:theater_id" => "schedules#get_date_thea"
    get "schedules/get_date_work/:id/:work_id" => "schedules#get_date_work"
    get "schedules/get_thea_work/:theater_id/:work_id" => "schedules#get_thea_work"
-   get "schedules/get_all_date/:id/:theater_id/:work_id" => "schedules#get_all_date"
+   get "schedules/get_all_data/:id/:theater_id/:work_id" => "schedules#get_all_data"
   end
 end
