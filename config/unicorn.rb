b6f15d11fe72f88bed441cd1a0055c9ba479fe73
hyuraku <yu@hill-wood.com>
2018-06-30 16:42:05 +0900

    change the name rail_root

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ app_path = File.expand_path('../../', __dir__)
 
 worker_processes 2
 
-ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
+ENV['BUNDLE_GEMFILE'] = app_path + "/Gemfile"
 
 working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
